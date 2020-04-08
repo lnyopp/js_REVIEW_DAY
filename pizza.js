897d4676087088c396096d7d6dc15be9d9b153fb
Rumble Roar <lnyopp15@gmail.com>
2020-04-08 09:47:42 -0400

    Pizza Exercise

---
+++ pizza.js
@@ -0,0 +1,11 @@
+
+
+function pizza() {
+    let pizzaToppings = ['Cheese','Pepperoni','Garlic','Peppers','Pinapple'];
+    let toppings = pizzaToppings.join(' and ');
+    
+    console.log('A delicious pizza with ' + toppings + '.');
+}
+
+
+pizza();
